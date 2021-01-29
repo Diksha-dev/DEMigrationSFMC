a503e9014ee1dad588ab5f6414ba88ab2032fe7e
01fazzy <faizal@cyntexa.com>
2021-01-29 19:34:12 +0530

    commit

--- index.js
+++ index.js
@@ -17,9 +17,136 @@ app.use(express.urlencoded({
   
 
   
+  var SourceAccessToken = '';
+  var SourceRestURL = '';
+  var DestinationAccessToken = '';
+  var DestinationRestURL = ''
+  
   app.post('/stack', (req, res) => {
-    console.log('Chal gya');
+    var SourceClientID = req.body.SourceClientID;
+    var SourceClientSecret = req.body.SourceClientSecret;
+    var SourceAuthBaseURI = req.body.SourceAuthBaseURI;
+    var SourceMID = req.body.SourceMID;
+  
+    var DestinationClientID = req.body.DestinationClientID;
+    var DestinationClientSecret = req.body.DestinationClientSecret;
+    var DestinationAuthBaseURI = req.body.DestinationAuthBaseURI;
+    var DestinationMID = req.body.DestinationMID;
+  
+    
+  
+    request.post({
+      headers: {'content-type' : 'application/json'},
+      url: SourceAuthBaseURI + '/v2/token',
+      body:{
+            'client_id': SourceClientID, //pass Client ID
+            'client_secret': SourceClientSecret, //pass Client Secret
+            'grant_type': 'client_credentials',
+            'account_id':SourceMID
+      },
+      json: true
+    }, 
+    function(error, response, body){
+      SourceAccessToken = body.access_token;
+      SourceRestURL = body.rest_instance_url;
+      console.log("Source Access : "+body.access_token);
+      console.log("ResponseURL : "+ body.rest_instance_url);
+  
+      //console.log("body" + body); 
+    });
+  
+  
+    request.post({
+      headers: {'content-type' : 'application/json'},
+      url: DestinationAuthBaseURI + '/v2/token',
+      body:{
+            'client_id': DestinationClientID, //pass Client ID
+            'client_secret': DestinationClientSecret, //pass Client Secret
+            'grant_type': 'client_credentials',
+            'account_id':DestinationMID
+      },
+      json: true
+    }, 
+    function(error, response, body){
+      DestinationAccessToken = body.access_token;
+      DestinationRestURL = body.rest_instance_url;
+      console.log("Destination Access : "+body.access_token);
+      console.log("ResponseURL : "+ body.rest_instance_url);
+      //console.log("body" + body); 
+    });
+  
+  
+
+
+
+
+
+    const SecondPage = path.join(__dirname, 'public', 'SecondPage.html');
+    res.sendFile(SecondPage);
+  
+    app.get("*", (req,res) => {
+      const SecondPage = path.join(__dirname, 'public', 'SecondPage.html');
+      res.sendFile(SecondPage);
+    });
+
+
+    app.use(express.urlencoded({
+      extended: true
+    }))
+    app.post('/stack1', (req, res) => {
+      var test = req.body.test;
+      console.log("test : " + test);
+  
+  
+      request.post({
+        headers: {
+          'Authorization' : "Bearer " + SourceAccessToken,
+          'content-type' : 'application/json'
+        },
+        url: SourceRestURL + '/asset/v1/content/assets/query',
+        body:{
+          "query": {
+            "property": "name",
+            "simpleOperator": "like",
+            "value": "Health Photo.jpg"
+          },
+          "fields": [
+            "id",
+            "name",
+            "enterpriseId",
+            "memberId",
+            "thumbnail",
+            "category",
+            "content",
+            "data",
+            "fileProperties"
+          ]
+        },
+        json: true
+      }, 
+      function(error, response, body){
+        console.log("JSON.stringify(body)" + JSON.stringify(body));
+        
+        console.log("JSON.stringify(response)" + JSON.stringify(response));
+  
+        console.log("JSON.stringify(error)" + JSON.stringify(error));
+      });
+  
+  
+  
+  
+  
+  
+  
+  
   })
+  
+  
+  
+  
+  
+  });
+  
   
   
   
