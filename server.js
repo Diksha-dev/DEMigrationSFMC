3aeb4d84382e0ebce8ece87d2887de99825274fe
01fazzy <faizal@cyntexa.com>
2021-02-01 16:47:37 +0530

    Commit

--- server.js
+++ server.js
@@ -51,9 +51,7 @@ app.use(express.urlencoded({
       SourceRestURL = body.rest_instance_url;
       console.log("Source Access : "+body.access_token);
       console.log("ResponseURL : "+ body.rest_instance_url);
-      console.log("Error : "+ error);
-  
-      //console.log("body" + body); 
+      console.log("body : "+ body); 
     });
   
   
@@ -73,7 +71,7 @@ app.use(express.urlencoded({
       DestinationRestURL = body.rest_instance_url;
       console.log("Destination Access : "+body.access_token);
       console.log("ResponseURL : "+ body.rest_instance_url);
-      console.log("Error : "+ error);; 
+      console.log("body : "+ body); 
     });
   
   
