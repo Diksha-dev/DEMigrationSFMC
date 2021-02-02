d51f26e162afa69c0736012527619a41c8a29159
01fazzy <faizal@cyntexa.com>
2021-02-02 19:10:21 +0530

    commit

--- server.js
+++ server.js
@@ -79,6 +79,61 @@ app.post('/Authenticate', (req, res) => {
 
 
 
+//List of Data Extension API Callout
+      let xmls = '<?xml version="1.0" encoding="UTF-8"?>\
+                    <s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">\
+                        <s:Header>\
+                            <a:Action s:mustUnderstand="1">Retrieve</a:Action>\
+                            <a:MessageID>urn:uuid:7e0cca04-57bd-4481-864c-6ea8039d2ea0</a:MessageID>\
+                            <a:ReplyTo>\
+                                <a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address>\
+                            </a:ReplyTo>\
+                            <a:To s:mustUnderstand="1">https://mc6vgk-sxj9p08pqwxqz9hw9-4my.soap.marketingcloudapis.com/Service.asmx</a:To>\
+                            <fueloauth xmlns="http://exacttarget.com">'+ 'eyJhbGciOiJIUzI1NiIsImtpZCI6IjEiLCJ2ZXIiOiIxIiwidHlwIjoiSldUIn0.eyJhY2Nlc3NfdG9rZW4iOiJZOWxxQkFSM2dKWVVBWWdyTWozT29iakEiLCJjbGllbnRfaWQiOiI0ZXBobjBxd2pldWF5b3N1YjA3cDNibmkiLCJlaWQiOjExMDAwNTY5MCwic3RhY2tfa2V5IjoiUzExIiwicGxhdGZvcm1fdmVyc2lvbiI6MiwiY2xpZW50X3R5cGUiOiJTZXJ2ZXJUb1NlcnZlciJ9.ELDRWfMSv-LLtJRJe3bZexewB7AJKir0JNJuP3eF95w.p7kDZhBWrs97D8-YQFXg7x7Upnf6CD9_-CgKzoWIbbUQYY6jZiSPE7Gn0fKvJZ27YYH0ZeDao555hG7nS99J86IZiN_HPIj6RZLFUzdMz9KKT20EkwfVNb7wKeK1p8fRXd5Plp2-9CX4-SBNFE9nLbWbXftyRV-bRFMg1xKmRJ3L8_ZY_Tk' +'</fueloauth>\
+                        </s:Header>\
+                        <s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">\
+                            <RetrieveRequestMsg xmlns="http://exacttarget.com/wsdl/partnerAPI">\
+                                <RetrieveRequest>\
+                                    <ObjectType>DataExtension</ObjectType>\
+                                    <Properties>CustomerKey</Properties>\
+                                    <Properties>Name</Properties>\
+                                    <Properties>DataExtension.ObjectID</Properties>\
+                                    <Properties>IsSendable</Properties>\
+                                    <Properties>SendableSubscriberField.Name</Properties>\
+                                    <Properties>SendableDataExtensionField.Name</Properties>\
+                                    <Properties>CategoryID</Properties>\
+                                    <Filter xsi:type="SimpleFilterPart">\
+                                        <Property>Name</Property>\
+                                        <SimpleOperator>equals</SimpleOperator>\
+                                        <Value>Filter Activity DE</Value>\
+                                    </Filter>\
+                                </RetrieveRequest>\
+                          </RetrieveRequestMsg>\
+                      </s:Body>\
+                    </s:Envelope>';
+
+      console.log('url in de list : ' + SourceSoapURL + 'Service.asmx');
+      console.log('SourceAccessToken in de list : ' + SourceAccessToken);
+      request.post({
+        url: 'https://mc6vgk-sxj9p08pqwxqz9hw9-4my.soap.marketingcloudapis.com/Service.asmx',
+        body: xmls
+      },
+      function (error, response, body) {
+        console.log("body : " + body);
+        console.log("error : " + error);
+      });
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -146,10 +201,10 @@ app.post('/Authenticate', (req, res) => {
         url: SourceSoapURL + 'Service.asmx',
         body: xmls
       },
-        function (error, response, body) {
-          console.log("body : " + body);
-          console.log("error : " + error);
-        });
+      function (error, response, body) {
+        console.log("body : " + body);
+        console.log("error : " + error);
+      });
     }
     await res.send('body');
   });
