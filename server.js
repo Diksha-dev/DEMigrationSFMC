032728d2936eac0831e047580facbc8a4ced7c91
01fazzy <faizal@cyntexa.com>
2021-02-02 17:36:58 +0530

    commit

--- server.js
+++ server.js
@@ -117,13 +117,8 @@ app.use(express.urlencoded({
       body: xmls
     }, 
     function(error, response, body){
-      console.log("JSON body : "+ JSON.stringify(body));
       console.log("body : "+ body); 
-
-      console.log("JSON response : "+ JSON.stringify(response));
       console.log("response : "+ response); 
-
-      console.log("JSON error : "+ JSON.stringify(error));
       console.log("error : "+ error); 
     });
 
