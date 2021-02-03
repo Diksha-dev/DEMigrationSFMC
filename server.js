b6ebb5f0198d12b89b6136ab00fcea8ff87e92d0
01fazzy <faizal@cyntexa.com>
2021-02-03 12:53:55 +0530

    commit

--- server.js
+++ server.js
@@ -21,9 +21,13 @@ app.use(express.urlencoded({
 var SourceAccessToken = '';
 var SourceRestURL = '';
 var SourceSoapURL = '';
+var SourceListDEResult;
+var SourceDEFieldsResult;
+
 var DestinationAccessToken = '';
 var DestinationRestURL = '';
 var DestinationSoapURL = '';
+
 
 app.post('/Authenticate', (req, res) => {
   var SourceClientID = req.body.SourceClientID;
@@ -108,48 +112,88 @@ app.post('/Authenticate', (req, res) => {
 
 
 
-  app.post("/DEListShowAPI", async function (req, res) {
+
+
+
+
+
+
+
+
+
+
+
+
+  async function getSourceListOfDE(){
+    await authTokenForBothSFDC();
+    var ListDEOption = {
+      'method': 'POST',
+      'url': SourceSoapURL + 'Service.asmx',
+      'headers': {
+        'Content-Type': 'text/xml',
+        'SoapAction': 'Retrieve',
+        'Authorization': 'Bearer ' + SourceAccessToken
+      },
+      body: '<?xml version="1.0" encoding="UTF-8"?>\r\n<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">\r\n    <s:Header>\r\n        <a:Action s:mustUnderstand="1">Retrieve</a:Action>\r\n        <a:MessageID>urn:uuid:7e0cca04-57bd-4481-864c-6ea8039d2ea0</a:MessageID>\r\n        <a:ReplyTo>\r\n            <a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address>\r\n        </a:ReplyTo>\r\n        <a:To s:mustUnderstand="1">https://mc6vgk-sxj9p08pqwxqz9hw9-4my.soap.marketingcloudapis.com/Service.asmx</a:To>\r\n        <fueloauth xmlns="http://exacttarget.com">'+ SourceAccessToken +'</fueloauth>\r\n    </s:Header>\r\n    <s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">\r\n        <RetrieveRequestMsg xmlns="http://exacttarget.com/wsdl/partnerAPI">\r\n            <RetrieveRequest>\r\n                <ObjectType>DataExtension</ObjectType>\r\n                <Properties>CustomerKey</Properties>\r\n                <Properties>Name</Properties>\r\n                <Properties>DataExtension.ObjectID</Properties>\r\n                <Properties>IsSendable</Properties>\r\n                      <Properties>CategoryID</Properties>\r\n                \r\n        \r\n                \r\n        \r\n             \r\n            </RetrieveRequest>\r\n      </RetrieveRequestMsg>\r\n   </s:Body>\r\n</s:Envelope>'
+
+    };
+    request(ListDEOption, function (error, response) {
+      if (error) throw new Error(error);
+      console.log('DE List :'+xmlParser.toJson(response.body));
+      SourceListDEResult = xmlParser.toJson(response.body);
+      var obj = {a: 1, b: 2};
+      for (var key in SourceListDEResult) {
+        if (obj.hasOwnProperty(key)) {
+        var val = obj[key];
+        console.log('val : '+ val);
+        }
+      }
+    });
+  }
+
+
+
+  async function getSourceDEFields(){
+    await authTokenForBothSFDC();
+    var DEFieldOption = {
+      'method': 'POST',
+      'url': SourceSoapURL + 'Service.asmx',
+      'headers': {
+        'Content-Type': 'text/xml',
+        'SoapAction': 'Retrieve',
+        'Authorization': 'Bearer ' + SourceAccessToken
+      },
+      body: '<?xml version="1.0" encoding="UTF-8"?>\r\n<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">\r\n    <s:Header>\r\n        <a:Action s:mustUnderstand="1">Retrieve</a:Action>\r\n        <a:MessageID>urn:uuid:7e0cca04-57bd-4481-864c-6ea8039d2ea0</a:MessageID>\r\n        <a:ReplyTo>\r\n            <a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address>\r\n        </a:ReplyTo>\r\n        <a:To s:mustUnderstand="1">https://mc6vgk-sxj9p08pqwxqz9hw9-4my.soap.marketingcloudapis.com/Service.asmx</a:To>\r\n        <fueloauth xmlns="http://exacttarget.com">' + SourceAccessToken + '</fueloauth>\r\n    </s:Header>\r\n    <s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">\r\n        <RetrieveRequestMsg xmlns="http://exacttarget.com/wsdl/partnerAPI">\r\n            <RetrieveRequest>\r\n                <ObjectType>DataExtensionField</ObjectType>\r\n                <Properties>Client.ID</Properties>\r\n                <Properties>CreatedDate</Properties>\r\n                <Properties>CustomerKey</Properties>\r\n                <Properties>DataExtension.CustomerKey</Properties>\r\n                <Properties>DefaultValue</Properties>\r\n                <Properties>FieldType</Properties>\r\n                <Properties>IsPrimaryKey</Properties>\r\n                <Properties>IsRequired</Properties>\r\n                <Properties>MaxLength</Properties>\r\n                <Properties>ModifiedDate</Properties>\r\n                <Properties>Name</Properties>\r\n                <Properties>ObjectID</Properties>\r\n                <Properties>Ordinal</Properties>\r\n                <Properties>Scale</Properties>\r\n\r\n                <Filter xsi:type="SimpleFilterPart">\r\n                    <Property>DataExtension.CustomerKey</Property>\r\n                    <SimpleOperator>equals</SimpleOperator>\r\n                    <Value>221ACA75-1A13-432E-9408-05F80B5BE733</Value>\r\n                </Filter>\r\n\r\n                <QueryAllAccounts>true</QueryAllAccounts>\r\n                <Retrieves />\r\n                <Options>\r\n                    <SaveOptions />\r\n                    <IncludeObjects>true</IncludeObjects>\r\n                </Options>\r\n            </RetrieveRequest>\r\n      </RetrieveRequestMsg>\r\n   </s:Body>\r\n</s:Envelope>'
+    
+    };
+    request(DEFieldOption, function (error, response) {
+      if (error) throw new Error(error);
+      console.log('DE Field :'+xmlParser.toJson(response.body));
+      SourceDEFieldsResult  = xmlParser.toJson(response.body);
+    });
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  app.post("/DEListShowAPI", async (req, res) => {
 
     if (req.body.reqForDEList = 'True') {
-      console.log('req.body : ' + JSON.stringify(req.body));
 
-      console.log('auth se pehle ');
-      await authTokenForBothSFDC();
-      console.log('auth k baad ');
-
-      var options = {
-        'method': 'POST',
-        'url': SourceSoapURL + 'Service.asmx',
-        'headers': {
-          'Content-Type': 'text/xml',
-          'SoapAction': 'Retrieve',
-          'Authorization': 'Bearer ' + SourceAccessToken
-        },
-        body: '<?xml version="1.0" encoding="UTF-8"?>\r\n<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">\r\n    <s:Header>\r\n        <a:Action s:mustUnderstand="1">Retrieve</a:Action>\r\n        <a:MessageID>urn:uuid:7e0cca04-57bd-4481-864c-6ea8039d2ea0</a:MessageID>\r\n        <a:ReplyTo>\r\n            <a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address>\r\n        </a:ReplyTo>\r\n        <a:To s:mustUnderstand="1">https://mc6vgk-sxj9p08pqwxqz9hw9-4my.soap.marketingcloudapis.com/Service.asmx</a:To>\r\n        <fueloauth xmlns="http://exacttarget.com">'+ SourceAccessToken +'</fueloauth>\r\n    </s:Header>\r\n    <s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">\r\n        <RetrieveRequestMsg xmlns="http://exacttarget.com/wsdl/partnerAPI">\r\n            <RetrieveRequest>\r\n                <ObjectType>DataExtension</ObjectType>\r\n                <Properties>CustomerKey</Properties>\r\n                <Properties>Name</Properties>\r\n                <Properties>DataExtension.ObjectID</Properties>\r\n                <Properties>IsSendable</Properties>\r\n                <Properties>SendableSubscriberField.Name</Properties>\r\n                <Properties>SendableDataExtensionField.Name</Properties>\r\n                <Properties>CategoryID</Properties>\r\n                \r\n        \r\n                <Filter xsi:type="SimpleFilterPart">\r\n                    <Property>Name</Property>\r\n                    <SimpleOperator>equals</SimpleOperator>\r\n                    <Value>Filter Activity DE</Value>\r\n                </Filter>\r\n        \r\n             \r\n            </RetrieveRequest>\r\n      </RetrieveRequestMsg>\r\n   </s:Body>\r\n</s:Envelope>'
-
-      };
-      request(options, function (error, response) {
-        if (error) throw new Error(error);
-        console.log(response.body);
-        console.log('JSON output', xmlParser.toJson(response.body));
-      });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      await getSourceListOfDE();
+      await getSourceDEFields();
+      
+      
       
     }
     await res.send('body');
