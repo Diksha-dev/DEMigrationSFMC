e0af98daee6f0d2e17fcd3547aa54c73a821c7d4
01fazzy <faizal@cyntexa.com>
2021-02-04 17:13:22 +0530

    commit

--- server.js
+++ server.js
@@ -40,6 +40,8 @@ app.post('/Authenticate', (req, res) => {
   var DestinationAuthBaseURI = req.body.DestinationAuthBaseURI;
   var DestinationMID = req.body.DestinationMID;
 
+  var selectedDEList;
+
 
   async function authTokenForBothSFDC(){
 
@@ -170,14 +172,31 @@ app.post('/Authenticate', (req, res) => {
         'SoapAction': 'Retrieve',
         'Authorization': 'Bearer ' + SourceAccessToken
       },
-      body: '<?xml version="1.0" encoding="UTF-8"?>\r\n<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">\r\n    <s:Header>\r\n        <a:Action s:mustUnderstand="1">Retrieve</a:Action>\r\n        <a:MessageID>urn:uuid:7e0cca04-57bd-4481-864c-6ea8039d2ea0</a:MessageID>\r\n        <a:ReplyTo>\r\n            <a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address>\r\n        </a:ReplyTo>\r\n        <a:To s:mustUnderstand="1">https://mc6vgk-sxj9p08pqwxqz9hw9-4my.soap.marketingcloudapis.com/Service.asmx</a:To>\r\n        <fueloauth xmlns="http://exacttarget.com">' + SourceAccessToken + '</fueloauth>\r\n    </s:Header>\r\n    <s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">\r\n        <RetrieveRequestMsg xmlns="http://exacttarget.com/wsdl/partnerAPI">\r\n            <RetrieveRequest>\r\n                <ObjectType>DataExtensionField</ObjectType>\r\n                <Properties>Client.ID</Properties>\r\n                <Properties>CreatedDate</Properties>\r\n                <Properties>CustomerKey</Properties>\r\n                <Properties>DataExtension.CustomerKey</Properties>\r\n                <Properties>DefaultValue</Properties>\r\n                <Properties>FieldType</Properties>\r\n                <Properties>IsPrimaryKey</Properties>\r\n                <Properties>IsRequired</Properties>\r\n                <Properties>MaxLength</Properties>\r\n                <Properties>ModifiedDate</Properties>\r\n                <Properties>Name</Properties>\r\n                <Properties>ObjectID</Properties>\r\n                <Properties>Ordinal</Properties>\r\n                <Properties>Scale</Properties>\r\n\r\n                <Filter xsi:type="SimpleFilterPart">\r\n                    <Property>DataExtension.CustomerKey</Property>\r\n                    <SimpleOperator>equals</SimpleOperator>\r\n                    <Value>221ACA75-1A13-432E-9408-05F80B5BE733</Value>\r\n                </Filter>\r\n\r\n                <QueryAllAccounts>true</QueryAllAccounts>\r\n                <Retrieves />\r\n                <Options>\r\n                    <SaveOptions />\r\n                    <IncludeObjects>true</IncludeObjects>\r\n                </Options>\r\n            </RetrieveRequest>\r\n      </RetrieveRequestMsg>\r\n   </s:Body>\r\n</s:Envelope>'
+      body: '<?xml version="1.0" encoding="UTF-8"?>\r\n<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">\r\n    <s:Header>\r\n        <a:Action s:mustUnderstand="1">Retrieve</a:Action>\r\n        <a:MessageID>urn:uuid:7e0cca04-57bd-4481-864c-6ea8039d2ea0</a:MessageID>\r\n        <a:ReplyTo>\r\n            <a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address>\r\n        </a:ReplyTo>\r\n        <a:To s:mustUnderstand="1">https://mc6vgk-sxj9p08pqwxqz9hw9-4my.soap.marketingcloudapis.com/Service.asmx</a:To>\r\n        <fueloauth xmlns="http://exacttarget.com">' + SourceAccessToken + '</fueloauth>\r\n    </s:Header>\r\n    <s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">\r\n        <RetrieveRequestMsg xmlns="http://exacttarget.com/wsdl/partnerAPI">\r\n            <RetrieveRequest>\r\n                <ObjectType>DataExtensionField</ObjectType>\r\n                <Properties>Client.ID</Properties>\r\n                <Properties>CreatedDate</Properties>\r\n                <Properties>CustomerKey</Properties>\r\n                <Properties>DataExtension.CustomerKey</Properties>\r\n                <Properties>DefaultValue</Properties>\r\n                <Properties>FieldType</Properties>\r\n                <Properties>IsPrimaryKey</Properties>\r\n                <Properties>IsRequired</Properties>\r\n                <Properties>MaxLength</Properties>\r\n                <Properties>ModifiedDate</Properties>\r\n                <Properties>Name</Properties>\r\n                <Properties>ObjectID</Properties>\r\n                <Properties>Ordinal</Properties>\r\n                <Properties>Scale</Properties>\r\n\r\n                                <QueryAllAccounts>true</QueryAllAccounts>\r\n                <Retrieves />\r\n                <Options>\r\n                    <SaveOptions />\r\n                    <IncludeObjects>true</IncludeObjects>\r\n                </Options>\r\n            </RetrieveRequest>\r\n      </RetrieveRequestMsg>\r\n   </s:Body>\r\n</s:Envelope>'
     
     };
     request(DEFieldOption, function (error, response) {
       if (error) throw new Error(error);
-      //console.log('DE Field :'+xmlParser.toJson(response.body));
-      SourceDEFieldsResult  = xmlParser.toJson(response.body);
+
+      SourceDEFieldsResult = response.body;
+      SourceDEFieldsResult = SourceDEFieldsResult.replace(/:/g, "");
+      SourceDEFieldsResult = xmlParser.toJson(SourceDEFieldsResult);
       SourceDEFieldsResult = JSON.parse(SourceDEFieldsResult);
+      SourceDEFieldsResult = SourceDEFieldsResult.soapEnvelope.soapBody.RetrieveResponseMsg;
+
+      for (var key in SourceDEFieldsResult) {
+        console.log('key : ' + SourceDEFieldsResult[key].Results.Name);
+        console.log('key : ' + SourceDEFieldsResult[key].Results.CustomerKey);
+        console.log('key : ' + SourceDEFieldsResult[key].Results.IsRequired);
+        console.log('key : ' + SourceDEFieldsResult[key].Results.IsPrimaryKey);
+        console.log('key : ' + SourceDEFieldsResult[key].Results.MaxLength);
+        console.log('key : ' + SourceDEFieldsResult[key].Results.DataExtension.CustomerKey);
+        console.log('key : ' + SourceDEFieldsResult[key].Results.FieldType);
+        console.log('Next');
+      }
+
+
+
     });
   }
 
@@ -229,14 +248,15 @@ app.post('/Authenticate', (req, res) => {
 
 
 
-
+  
   app.post("/SelectedDEList", async (req, res) => {
     
     if (req.body.reqForSelectedDEList) {
-      console.log('reqForSelectedDEList : '+ req.body.reqForSelectedDEList);
+      selectedDEList = req.body.reqForSelectedDEList;
+      console.log('reqForSelectedDEList : '+ selectedDEList);
 
       //SourceListDEResult = await getSourceListOfDE();
-      //await getSourceDEFields();
+      getSourceDEFields();
     }
     res.send('reqForSelectedDEList');
 
