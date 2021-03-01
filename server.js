0042a296f20ffa5869b67dea3b7f667f177d1921
01fazzy <faizal@cyntexa.com>
2021-03-01 18:13:50 +0530

    commit

--- server.js
+++ server.js
@@ -1294,7 +1294,7 @@ app.post('/Authenticate', (req, res) => {
   async function getMoreSharedDEData(NextUrl , key , i) {
     return new Promise(async function (resolve, reject) {
       //var url = SourceRestURL + 'data/v1/customobjectdata/token/' + NextUrl + '/rowset?$page=' + i;
-      console.log('url : ' + url);
+      //console.log('url : ' + url);
       var SharedDEMoreDataOptions = {
         'method': 'GET',
         'url': SourceRestURL + 'data' + NextUrl,
