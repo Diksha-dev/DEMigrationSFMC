cca86bbc494f6c6f1e2c7997193eb43944005648
01fazzy <faizal@cyntexa.com>
2021-03-03 15:53:42 +0530

    commit

--- server.js
+++ server.js
@@ -814,7 +814,7 @@ app.post('/Authenticate', (req, res) => {
   }
 
   async function insertDEDataToDestination(key) {
-    return new Promise(function (resolve, reject) {
+    return new Promise(async function (resolve, reject) {
       if(DEListMap[key].DEDataMap.length != 0) {
         if(DEListMap[key].DEDataMap.length <= 10000) {
           if(DEListMap[key].DEDataMap[0].keys.size != 0) {
